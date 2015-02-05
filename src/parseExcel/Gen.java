516e9eb8911393824c9fda451976f645881b15d4
yutuer <yutuerzf@163.com>
2015-02-06 00:48:40 +0800

    11

--- src/parseExcel/Gen.java
+++ src/parseExcel/Gen.java
@@ -1,7 +1,10 @@
 package parseExcel;
 
+import java.util.List;
+
 public class Gen {
 	public static void main(String[] args) {
-		
+		MultiMap<String, List<ExcelHead>> map = ExcelUtil.getExcelBeans(false,
+				"RankModel.xlsx");
 	}
 }

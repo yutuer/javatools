400ca9fb6c1697b417f1d4dcd177f4b2bf2f857e
yutuer <yutuerzf@163.com>
2015-02-16 22:40:38 +0800

    11232

--- src/spring/T.java
+++ src/spring/T.java
@@ -3,10 +3,15 @@ package spring;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 public class T {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("redis.xml");
 		RedisTemplate redisTemplate = (RedisTemplate) ctx.getBean("jedisTemplate");
+		ValueOperations<String, User> valueOper = redisTemplate.opsForValue();  
+        User suser = new User(1,"zhangsan",12);  
+        valueOper.set("user:" + suser.getId(),suser);  
+        System.out.println(valueOper.get("user:" + suser.getId()).getName());
 	}
 }

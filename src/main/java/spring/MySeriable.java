6e752d7f87acca0cab4133f8419c6dc47afa0a2c
yutuer <yutuerzf@163.com>
2015-03-09 14:37:07 +0800

    1

---
+++ src/main/java/spring/MySeriable.java
@@ -0,0 +1,24 @@
+package spring;
+
+import io.netty.handler.codec.marshalling.MarshallingDecoder;
+import io.netty.handler.codec.marshalling.MarshallingEncoder;
+
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.SerializationException;
+
+public class MySeriable implements RedisSerializer {
+
+	private MarshallingDecoder decoder = MarshallingCodeCFactory.buildMarshallingDecoder();
+	private MarshallingEncoder encoder = MarshallingCodeCFactory.buildMarshallingEncoder();
+
+	@Override
+	public byte[] serialize(Object t) throws SerializationException {
+		return null;
+	}
+
+	@Override
+	public Object deserialize(byte[] bytes) throws SerializationException {
+		return null;
+	}
+
+}

adb8e1d8ca01582b02d8ff4521e57a40b0d0b167
yutuer <yutuerzf@163.com>
2015-04-17 10:41:02 +0800

    1

---
+++ src/main/java/test/App.java
@@ -0,0 +1,20 @@
+package test;
+
+import io.netty.util.internal.PlatformDependent;
+
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+		AtomicReferenceFieldUpdater updater = PlatformDependent.newAtomicReferenceFieldUpdater(Dog.class, "name");
+		Dog dog1 = new Dog();
+		updater.compareAndSet(dog1, dog1.name, "test");
+		System.out.println(dog1.name);
+
+	}
+}
+
+class Dog {
+	volatile String name = "dog1";
+
+}

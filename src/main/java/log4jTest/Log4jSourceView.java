4a72b30128cd08093cfe81db0b19b1299bdd75a8
yutuer <yutuerzf@163.com>
2017-08-30 23:58:25 +0800

    log4jTest

---
+++ src/main/java/log4jTest/Log4jSourceView.java
@@ -0,0 +1,19 @@
+package log4jTest;
+
+import java.net.URL;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.spi.LoggerRepository;
+
+public class Log4jSourceView {
+	
+	public static void main(String[] args) {
+		URL resource = Thread.currentThread().getContextClassLoader().getResource("log4j.properties");
+		LoggerRepository loggerRepository = LogManager.getLoggerRepository();
+		new PropertyConfigurator().doConfigure(resource.getPath(), loggerRepository);
+		Logger rootLogger = loggerRepository.getRootLogger();
+		rootLogger.info("1111");
+	}
+}

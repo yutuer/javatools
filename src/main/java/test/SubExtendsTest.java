46cadc48dada80707430303e1826a18c48499535
yutuer <yutuerzf@163.com>
2017-12-26 10:37:38 +0800

    tmp 1

--- src/main/java/test/SubExtendsTest.java
+++ src/main/java/test/SubExtendsTest.java
@@ -25,13 +25,13 @@ public class SubExtendsTest {
 
 	private static class Sub extends Base {
 		
-		private int i;
 		//这种调用其实很危险, 因为不允许this类有多余的属性, 如果有,那么window调用的时候, this还没初始化完毕
 		private Window window = new Window(this); 
+		
+		private int i;
 
 		public Sub() {
-			this.i = 10;
-			System.out.println("in Sub Construct");
+			System.out.println("in Sub Construct, i=" + i);
 		}
 
 		public void SI() {
@@ -41,6 +41,7 @@ public class SubExtendsTest {
 		@Override
 		public void subM() {
 			System.out.println(window);
+			this.i = 10;
 		}
 
 		public HashMap doStuff(Map map) {
@@ -55,9 +56,8 @@ public class SubExtendsTest {
 
 		public Window(Sub sub) {
 			this.sub = sub;
-			System.out.println("in window " + sub.i);
+			System.out.println("in window, sub.i:" + sub.i);
 			sub.SI();
-			
 		}
 
 		@Override
@@ -70,12 +70,6 @@ public class SubExtendsTest {
 	public static void main(String[] args) {
 		Base b = new Sub();
 		// b.doStuff(new HashMap<>());
-		
-		
-		Date date = new Date(1510629952266L);
-		System.out.println(date);
-		
-		System.out.println(432000 / 3600 /24);
 	}
 
 }
